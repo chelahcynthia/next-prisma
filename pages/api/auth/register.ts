fe678d1b4791e97aaae472ac75c44d90af6127ea
Chelah <cchelangat989@gmail.com>
2025-01-27 10:16:41 +0300

    validate inputs

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,13 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 import bcrypt from 'bcrypt';
+import { error } from "console";
 
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST' ) {
         const {email, full_name, password, phone_number} = req.body;
-
+        // validate the input
+        if(!email || !full_name || !password || !phone_number){
+            return res.status(400).json({error: "All fields are required, friend!"});
+        }
+     
     }
-    // hash the password
-    const password_hash =  await bcrypt.hash(password, 10)
+    
 }
