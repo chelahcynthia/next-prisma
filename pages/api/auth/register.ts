61e9e37cf050914c40ad9a2ae1a1921a60845004
Chelah <cchelangat989@gmail.com>
2025-01-27 09:40:40 +0300

    handle http requests

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,3 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 import bcrypt from 'bcrypt';
+
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST' ) {
+        const (email,full_name, password, phone_number) = req.body;
+    }
+}
