4efc3db4ff06b5db5595b303117c7f402802b4cf
Chelah <cchelangat989@gmail.com>
2025-01-27 09:53:52 +0300

    hash password

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -5,6 +5,9 @@ import bcrypt from 'bcrypt';
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST' ) {
-        const (email,full_name, password, phone_number) = req.body;
+        const {email, full_name, password, phone_number} = req.body;
+
     }
+    // hash the password
+    const password_hash =  await bcrypt.hash(password, 10)
 }
