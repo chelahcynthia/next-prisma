201701dbfaa7ddb024f29697baa95437aeedd345
Chelah <cchelangat989@gmail.com>
2025-01-28 12:05:34 +0300

    create function that handles signin

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import bcrypt from 'bcryptjs'
+import prisma from '@/lib/prisma'
+
+export async function POST(request: Request) {
+    try{
+
+    }catch(error) {
+
+    }
+}
