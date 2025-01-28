51d0f9890551a5261d3d87c4bd7c22ff405b479b
Chelah <cchelangat989@gmail.com>
2025-01-28 12:45:55 +0300

    ensure all required fields are provided

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,9 +1,20 @@
 import { NextResponse } from "next/server";
 import bcrypt from 'bcryptjs'
 import prisma from '@/lib/prisma'
+import { error } from "console";
 
 export async function POST(request: Request) {
     try{
+        const body = await request.json()
+        const { email, full_name, password, phone_number} = body
+
+        // validations
+        if (!email || !full_name || !password || !phone_number) {
+            return NextResponse.json(
+                { error: 'Missing required fields'},
+                { status: 400}
+            )
+        }
 
     }catch(error) {
 
